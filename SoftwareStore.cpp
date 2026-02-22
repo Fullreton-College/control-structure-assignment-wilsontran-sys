bffec33bfac5785a0a27fcc9d6186e7683d4a067
Tran, Wilson <wdtran2018@gmail.com>
2026-02-21 21:39:23 -0800

    Added prompt to get user input, decision flow for determining discount rate, logic processing to calculate total cost, and output statements

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
+  const double RETAIL_COST = 199.00;
 
-    //express pace in minutes and seconds
-  /* write your code here */
+  int quantity;
+  double discountRate = 0.00;
+  double cost;
+  double totalCost;
 
-  //express your output in 1 decimal points
+  cout << "Enter the number of units purchased: ";
+  cin >> quantity;
 
+  if(quantity >= 10 && quantity < 20)
+  {
+    discountRate = 0.20;
+  }
+  else if(quantity >= 20 && quantity <= 40)
+  {
+    discountRate = 0.30;
+  }
+  else if(quantity >= 50 && quantity < 100)
+  {
+    discountRate = 0.40;
+  }
+  else if(quantity >= 100)
+  {
+    discountRate = 0.50;
+  }
 
+  cost = quantity * RETAIL_COST;
+  totalCost = cost * (1.00 - discountRate);
+
+  cout << "The total cost of the purchase is: $" << fixed << setprecision(2) << cost << endl;
 }
