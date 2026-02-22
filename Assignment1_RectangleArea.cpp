4fdc2aa792674c03ee733e35d590d25c79967d6c
Tran, Wilson <wdtran2018@gmail.com>
2026-02-21 21:02:36 -0800

    Added input prompts. logic processing, and decision structures for ouput

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -4,7 +4,41 @@ using namespace std;
 
 int main()
 {
-	
+	int lengthOne;
+    int widthOne;
+    int lengthTwo;
+    int widthTwo;
+    int areaOne;
+    int areaTwo;
+
+    cout << "Enter the length of the first rectangle: ";
+    cin >> lengthOne;
+    cout << "Enter the width of the first rectangle: ";
+    cin >> widthOne;
+
+    cout << "Enter the length of the second rectangle: ";
+    cin >> lengthTwo;
+    cout << "Enter the width of the second rectangle: ";
+    cin >> widthTwo;
+
+    cout << endl;
+
+    areaOne = lengthOne * widthOne;
+    areaTwo = lengthTwo * widthTwo;
+
+    if(areaOne > areaTwo)
+    {
+        cout << "The 1st rectangle has the greater area" << endl;
+    }
+    else if(areaOne < areaTwo)
+    {
+        cout << "The 2nd rectangle has the greater area" << endl;
+    }
+    else
+    {
+        cout << "The areas are the same" << endl;
+    }
+
     return 0;
 }
 
